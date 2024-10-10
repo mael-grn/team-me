623c843d89e73b6a655226d3a6d8f6dafede2f7f
mael <maelgarnier5@gmail.com>
2024-10-09 22:31:26 -0400

    maj styles

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,11 @@ import Link from 'next/link';
 import StorageManager from './tools/storageManager';
 import * as motion from "framer-motion/client"
 import {useEffect, useState} from "react";
+import {router} from "next/client";
 
 export default function Home() {
     const [scrolled, setScrolled] = useState(false);
-
+    const [animateForward, setAnimateForward] = useState(false);
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 50) {
@@ -19,39 +20,40 @@ export default function Home() {
             }
         };
 
+
+
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    const handleLink = (link) => {
+        setAnimateForward(true);
+        setTimeout(() => {
+            window.location.href = link;
+        }, 900);
+    }
   return (
-    <div>
+    <div className={animateForward && styles.forward}>
         <main className={scrolled && styles.scrolled}>
             <p className={styles.appname}>Team Me</p>
             <div className={styles.heroDiv}>
                 <div className={styles.heroLeft}>
-                    <h1>Découvrez une nouvelle manière de gérer vos équipes</h1>
+                    <h1>Découvrez une toute nouvelle manière de gérer vos équipes</h1>
                     <p>Etes-vous prêt pour embarquer dans une toute nouvelle manière de manager vos équipes sportives
                         ?</p>
                     <div className={styles.buttonsDiv}>
-                        <Link href={"/register"} className={styles.startBtn}>Commencer</Link>
-                        <Link href={"/login"} className={styles.loginBtn}>Connexion</Link>
+                        <a onClick={() => handleLink("/register")} className={styles.startBtn}>Commencer</a>
+                        <a onClick={() => handleLink("/login")} className={styles.loginBtn}>Connexion</a>
                     </div>
                 </div>
-                <div className={styles.heroRight}>
-                    <Image
-                        src="/assets/running2.jpg"
-                        alt="Hero"
-                        width={3198}
-                        height={2304}
-                    />
-                </div>
-            </div>
-
-            <div className={styles.section}>
-                    <h1>Une solution pensée et créée pour les sportifs</h1>
-                    <p>Team Me est une application permettant aux coach sportifs de gerer leurs équipes, de gagner du
-                        temps et d'avoir une meilleure vision d'ensemble.</p>
+                <Image
+                    src="/assets/running.svg"
+                    alt="Hero"
+                    width={1000}
+                    height={1000}
+                />
             </div>
 
         </main>
